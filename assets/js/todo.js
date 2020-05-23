e76c7635b91a3add3adf99ae31fc5b51d0f5d2ce
harikrishnan-a-k <harikrishnan.a.k.c@gmail.com>
2020-05-23 11:25:36 +0530

    added full todo list functionalityusing jquery and css

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -17,12 +17,36 @@ $('.listContainer ul.demo').on('click','span.delete',function(e){
         $(this).remove();
     })
 });
-
+// adding new <li> on enter keypress
 $('#inputText').keypress(function(e){
     if(e.which===13){
         let text=$(this).val();
-        $('.listContainer ul').append(`<li><span class="delete">X</span> <span class="todo text-capitalize">${text}</span> </li>`);
+        $('.listContainer ul').append(`<li><span class="delete"><i class="fas fa-trash-alt"></i></span> <span class="todo text-capitalize">${text}</span> </li>`);
         $(this).val('');
     }
 })
 
+//toggle inputText on clicking pencil icon
+$('.addButton').on('click',()=>{
+    $('#inputText').fadeToggle(800);
+})
+
+//toggle todoList title to input for changing when click on it 
+$(function () {
+    $(document).on('click', 'span#todoTitle', function () {
+        var input = $('<input />', {
+            'type': 'text',
+                'name': 'unique',
+                'id':'todoTitleInput',
+                'value': $(this).html()
+        });
+        $(this).parent().append(input);
+        $(this).remove();
+        input.focus();
+    });
+
+    $(document).on('blur', 'input#todoTitleInput', function () {
+        $(this).parent().append($('<span />').attr('id','todoTitle').html($(this).val()));
+        $(this).remove();
+    });
+});
